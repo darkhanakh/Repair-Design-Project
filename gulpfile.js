529b87b662d16a309306660c08b2f4d6a5af1d99
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-05-07 21:10:37 +0600

    feat: add Futura Book and Demon fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
-const {
-  src,
-  dest,
-  watch
-} = require("gulp");
+const { src, dest, watch } = require("gulp");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 function bs() {
   serveSass();
@@ -14,16 +11,20 @@ function bs() {
     },
   });
   watch("./*.html").on("change", browserSync.reload);
-  watch("./sass/**/*.sass", serveSass)
+  watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on("change", browserSync.reload);
-};
-
+}
 
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass")
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
-};
+}
 
 exports.serve = bs;
