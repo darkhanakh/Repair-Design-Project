fa8349c3c22291761a041ce3d7f944df6f8ad448
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-05-07 21:11:55 +0600

    refact: add scss files to watch in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,12 @@ function bs() {
   });
   watch("./*.html").on("change", browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on("change", browserSync.reload);
 }
 
 function serveSass() {
-  return src("./sass/**/*.sass")
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
     .pipe(sass())
     .pipe(
       autoprefixer({
