ef8be53253e6be2f91fada3a3f67ee25ee0fd607
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-05-14 11:05:10 +0600

    refact: refact JavaScript script into JQuery script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+/* document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
   const modalButton = document.querySelectorAll('[data-toggle=modal]');
   const closeModal = document.querySelector('.modal__close');
@@ -12,4 +12,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 
   closeModal.addEventListener('click', switchModal);
+}); */
+
+$(document).ready(function () {
+  var modal = $(".modal"),
+    modalButton = $("[data-toggle=modal]"),
+    closeModal = $(".modal__close");
+
+  modalButton.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
+
+  closeModal.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
 });
