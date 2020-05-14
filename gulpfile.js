791ab1e9aaf465f46f1be00493346e4a7885a115
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-05-14 18:34:19 +0600

    feat: added vide jquery plagin and delete dist and src folders

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+function bs() {
+  serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  watch("./*.html").on("change", browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on("change", browserSync.reload);
+}
+
+function serveSass() {
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
+}
+
+exports.serve = bs;
---
+++ js/main.js
@@ -0,0 +1,41 @@
+/* document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector('.modal');
+  const modalButton = document.querySelectorAll('[data-toggle=modal]');
+  const closeModal = document.querySelector('.modal__close');
+
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  };
+
+  modalButton.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeModal.addEventListener('click', switchModal);
+}); */
+
+$(document).ready(function () {
+  var modal = $(".modal"),
+    modalButton = $("[data-toggle=modal]"),
+    closeModal = $(".modal__close");
+
+  modalButton.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
+
+  closeModal.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
+
+  function smoothScroolHome() {
+    $('#js-arrow').on('click', function (event) {
+      event.preventDefault();
+      var top = $('#js-hero').offset().top;
+
+      $("html, body").animate({
+        scrollTop: top
+      }, 500);
+    });
+  }
+  smoothScroolHome();
+});
